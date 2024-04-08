7b2929cc94a3d3874a751d1a38fe6240bfbd20e4
matviylevicky <levicky2009@gmail.com>
2024-04-08 18:55:13 +0300

    готове дз

---
+++ src/js/cript.js
@@ -0,0 +1,46 @@
+// завдання номер 1 
+const celsius = 12;
+const fahrenheit = celsius * 1.8 + 32;
+console.log( fahrenheit);
+// завдання номер 2
+const days = 30;
+const hours = days * 24;
+const minuts = days * 1440;
+console.log(hours );
+console.log(minuts);
+//завдання номер 3 
+const hp = 100;
+const energy = 200;
+const hpTwo = hp - 40;
+const energyTwo = energy - 50;
+console.log(hpTwo);
+console.log(energyTwo);
+//завдання номер 4
+const sum = 200;
+const discount = sum * 0.1;
+console.log(discount)
+//завдання номер 5
+const number = 0.1 + 0.2;
+const floor = Math.floor(number);
+console.log(floor)
+//завдання номер 6
+const stringNumber = "0.1 + 0.2";
+const normalNumber = parseFloat(stringNumber)
+console.log(normalNumber)
+//завдання номер 7 
+const stringNumberTwo = "10ggfjeif";
+const numberTwo = parseInt(stringNumberTwo);
+console.log(numberTwo)
+//завдання номер 8
+const startNumber = 25;
+const endNumber = Math.sqrt(startNumber);
+console.log(endNumber);
+//завдання номер 9
+const numberString = "20jdlerifja";
+const intNumber = parseInt(numberString);
+console.log(intNumber);
+
+const normalNumberTwoo = 10000;
+const toNumber = toString(normalNumberTwoo);
+console.log(typeof toNumber);
+
